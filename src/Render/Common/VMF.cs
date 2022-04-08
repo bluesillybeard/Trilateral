33e860eb2e56633465d24d7873f6f35dc4fe1d90
bluesillybeard <bluesillybeard2@gmail.com>
2022-04-07 20:53:50 -0600

    progress on vmf

---
+++ src/Render/Common/VMF.cs
@@ -0,0 +1,5 @@
+namespace Voxelesque.Render.Common{
+    class VMF{
+        
+    }
+}
---
+++ src/Render/Common/VModel.cs
@@ -0,0 +1,6 @@
+namespace Voxelesque.Render.Common{
+    class VMFModel{
+        float[] vertices; //elements: xpos, ypos, zpos, xtex, ytex, xnorm, ynorm, znorm
+        uint[] indices;
+    }
+}
