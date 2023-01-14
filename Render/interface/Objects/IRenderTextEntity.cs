0bc6359381d05baeab2e55c253d11d665ef75117
bluesillybeard <bluesillybeard2@gmail.com>
2023-01-13 20:44:21 -0700

    refactor VRender into a submodule

--- Render/interface/Objects/IRenderTextEntity.cs
+++
@@ -1,7 +0,0 @@
-namespace Render;
-
-public interface IRenderTextEntity : IRenderEntity{
-    string Text{get;set;}
-    bool CenterX{get;set;}
-    bool CenterY{get;set;}
-}
--- Render/interface/Objects/IRenderTexture.cs
+++
@@ -1,6 +0,0 @@
-namespace Render{
-    //this interface exists solely to take advantage of OOP.
-    public interface IRenderTexture: IRenderObject{
-
-    }
-}
