ccfdd64ff532d317c0e3fbceca6bbbb4156d6965
bluesillybeard <bluesillybeard2@gmail.com>
2023-07-16 01:17:07 -0600

    door screen transparency (seems) complete

--- Trilateral/Resources/shaders/ChunkShader/fragment.glsl
+++ Trilateral/Resources/shaders/ChunkShader/fragment.glsl
@@ -35,6 +35,47 @@ uint xorshift32(uint v)
     return v;
 }
 
+uint xorshift323d(uint x, uint y, uint z)
+{
+    uint v = 0u;
+    v ^= xorshift32(x);
+    v ^= xorshift32(y);
+    v ^= xorshift32(z);
+    return v * v;
+}
+
+uint xorshift32extreme(uint v)
+{
+    v ^= v >> 17u;
+    v ^= v << 13u;
+    v ^= v >> 5u;
+    v ^= v << 21u;
+    v ^= v >> 7u;
+    v ^= v << 27u;
+    v ^= v >> 31u;
+    v ^= v << 19u;
+    v ^= v >> 11u;
+    v ^= v << 3u;
+    v ^= v >> 29u;
+    v ^= v >> 25u;
+    v ^= v << 1u;
+    v ^= v << 15u;
+    v ^= v >> 9u;
+    v ^= v << 23u;
+    v ^= v * 3u;
+    return v;
+}
+
+uint xorshift32extreme3d(uint x, uint y, uint z)
+{
+    uint v = 0u;
+    v ^= xorshift32extreme(x);
+    v ^= xorshift32extreme(y);
+    v ^= xorshift32extreme(z);
+    //v = xorshift32extreme(v);
+    return v;
+}
+
 uint reverseBits(uint v)
 {
     // swap odd and even bits
@@ -46,29 +87,58 @@ uint reverseBits(uint v)
     // swap bytes
     v = ((v >> 8) & 0x00FF00FFu) | ((v & 0x00FF00FFu) << 8);
     // swap 2-byte long pairs
-    v = ( v >> 16             ) | ( v               << 16);
+    v = ( v >> 16              ) | ( v                << 16);
 
     return v;
 }
 
 
-//TODO: figure out how to fix this so it's actually normalized
 float bayerMatrix2D(uint i, uint j)
 {
-    const uint n = 8u;
+    const uint n = 256u;
     i = i % n;
     j = j % n;
-    return reverseBits(interleave(i ^ j, i)) / float(64);
+    //this differs from the origial formula.
+    // Probably because I did something wrong lol.
+    // But, it produces an acceptable result.
+    return reverseBits(interleave(i ^ j, i)) / float(4294967295u);
+}
+
+const float gammaScale = 5;
+//GLSL isn't smart enough to know that 2 is a positive number...
+// And yet, it is well aware that exp is purely functional.
+// GLSL, why are you this way?
+// (probably because it's a pretty old language, developed right near the beginning of GPU computation)
+const float exps = exp(gammaScale);
+
+float gammaCorrect(float input)
+{
+    // TODO: test if my weird function or actual gamma correction is faster.
+    // Gamma correction is a simpler formula, but the exp() function might be extremely highly optimized.
+    
+    return (exp(gammaScale*input)-1)/(exps-1);
 }
 
 float getThreshold(vec3 input)
 {
-    //return xorshift32(floatBitsToUint(input.x + input.y * 5 + input.z * 11)) / 4294967296.0;
-    const uint depthScale = 32768u;
-    uint x = uint(input.x);
-    uint y = uint(input.y);
-    uint z = uint(input.z*depthScale);
-    return bayerMatrix2D(x+z*2u, y+z);
+    float value = xorshift323d(floatBitsToUint(input.x), floatBitsToUint(input.y), floatBitsToUint(input.z)) / 4294967296.0;
+    return 1 - gammaCorrect(value);
+
+    // //Nobody's ever done a 3D bayer matrix before (at least not on the internet),
+    // //so I was on my own in terms of figuring this out.
+    // // GLSL is so annoying when using unsigned integers - it's too dumb to see that 2 is positive.
+    // // Most languages with unsigned its are smart enough to see that. GLSL is not.
+    // const uint depthScale = uint(1 >> 20);
+    // uint x = uint(input.x);
+    // uint y = uint(input.y);
+    // uint z = xorshift32(floatBitsToUint(input.z));
+    // //That by itself would work, however when multiple transparent objecs overlap,
+    // // they tend to activate the same pixels, so fewer pixels are added than would normally make sense.
+    // // So, one last modification tries to fix that.
+    // x += z / depthScale;
+    // y += xorshift32(z) / depthScale;
+    
+    // return bayerMatrix2D(x, y)/3 + bayerMatrix2D(x, z)/3 + bayerMatrix2D(y, z)/3;
 }
 
 out vec4 colorOut;
@@ -76,7 +146,10 @@ in vec2 texCoord;
 in float depth;
 uniform sampler2D tex;
 void main(){
-    colorOut = vec4(getThreshold(vec3(gl_FragCoord.xy, depth)));//texture(tex, texCoord);
-    
-    //if(colorOut.a < getThreshold(screenPos))discard;
+    colorOut = texture(tex, texCoord);
+    //colorOut = vec4(vec3(getThreshold(vec3(gl_FragCoord.xy, depth))), 1);
+    if(colorOut.a == 0 || (colorOut.a != 1 && colorOut.a < getThreshold(vec3(gl_FragCoord.xy, depth))))
+    {
+        discard;
+    }
 }
